11a9bc3fa7ef49f7bf6853fce0d8651d8f15ed43
Alex <adonesky@gmail.com>
2022-01-20 17:19:22 -0600

    making some progress

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1075,7 +1075,6 @@ export default class MetamaskController extends EventEmitter {
       alertController,
       approvalController,
       appStateController,
-      assetsContractController,
       collectiblesController,
       collectibleDetectionController,
       assetsContractController,
